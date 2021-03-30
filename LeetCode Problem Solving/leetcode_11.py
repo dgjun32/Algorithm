3515ff0102fc4f29d8a288ea67f4fed983ef6b13
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-03-30 19:03:57 +0900

    Update leetcode_11.py

--- LeetCode Problem Solving/leetcode_11.py
+++ LeetCode Problem Solving/leetcode_11.py
@@ -1,6 +1,8 @@
+#1. Bruto Force Approach
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         maximum = 0
+        #for all left / right index pairs which satisfy left < right, compute Area and update maximum value
         for left in range(len(height)-1):
             for right in range(left+1, len(height)):
                 h = min(height[left], height[right])
@@ -8,4 +10,25 @@ class Solution:
                 if amt > maximum:
                     maximum = amt
         return maximum
- #time limit exceeded 
+ #time limit exceeded(Intuitive but inefficient) 
+
+#2. Solution Using Two Pointer
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #setting left pointer at start of the list, right pointer at end of the list 
+        maximum = 0
+        left, right = 0, len(height)-1
+        while left < right:
+            amt = min(height[left], height[right]) * (right - left)
+            #update maximum value
+            if amt > maximum:
+                maximum = amt
+            #move each pointer
+            if height[left] < height[right]:
+                left += 1
+            elif height[left] > height[right]:
+                right -= 1 
+            else:
+                left += 1
+                right -= 1
+        return maximum
