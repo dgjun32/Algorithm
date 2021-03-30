a969a4365ddeda8cb85c46e9e9fc2086e4d45a4d
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-03-30 19:05:55 +0900

    Update and rename leetcode_11.py to leetcode_11_Container with Most Water.py
    Container with Most Water

--- LeetCode Problem Solving/leetcode_11_Container with Most Water.py
+++ LeetCode Problem Solving/leetcode_11_Container with Most Water.py
@@ -1,3 +1,8 @@
+# Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+# n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0).
+# Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+# Notice that you may not slant the container.
+
 #1. Bruto Force Approach
 class Solution:
     def maxArea(self, height: List[int]) -> int:
