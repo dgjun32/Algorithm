457e301fc672e8086f711c89fdab764d9884d128
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-04-19 07:38:50 +0900

    Update leetcode 223_Rectangle Area.py

--- LeetCode Problem Solving/math/leetcode 223_Rectangle Area.py
+++ LeetCode Problem Solving/math/leetcode 223_Rectangle Area.py
@@ -1,4 +1,6 @@
 # Given the coordinates of two rectilinear rectangles in a 2D plane, return the total area covered by the two rectangles.
+
+#solution 1. 
 class Solution:
     def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
         x = [A,C,E,G]
@@ -13,3 +15,16 @@ class Solution:
         else:
             intersect = (x[2]-x[1]) * (y[2]-y[1])
             return each_sum - intersect
+
+#solution 2.
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        each_sum = (D-B) * (C-A) + (H-F) * (G-E) # Find area covered by second rectangle
+        instersect = 0 # do they some intersect?
+        if A <= G and C >= E and B <= H and D >= F:
+            # Yes! There is some overlap.
+            width = min(C, G) - max(A,E) # Width of the overlap portion
+            height = min(D, H) - max(B,F) # Height of the overlap portion
+            intersect = width * height # Area of the overlap portion
+        
+        return each_sum - intersect
