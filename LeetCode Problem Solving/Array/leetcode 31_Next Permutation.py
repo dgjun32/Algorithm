fbadd5e20ee3b4fd2d431a8bd64aac16e30598bc
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-04-24 14:08:17 +0900

    Update leetcode 31_Next Permutation.py

--- LeetCode Problem Solving/Array/leetcode 31_Next Permutation.py
+++ LeetCode Problem Solving/Array/leetcode 31_Next Permutation.py
@@ -1,25 +1,21 @@
-def nextPermutation(nums):
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         keep = True
         i = len(nums) - 2
         if len(nums) >= 2:
             while keep == True:
-                temp = nums[i:] #look at i th index of list to last index of list(window). I will move start point of window to left sequentially.
-                #if we have seen all part of the list and still keep == True, it implies that list is sorted in reverse(e.x. [4,3,2,1]) -> sort it and stop iterating
                 if i < 0:
                     nums.sort()
                     keep = False
-                #elif temp list is sorted in reverse order, there is nothing to change -> let's move window to left
-                elif temp == sorted(temp, reverse = True):
+                elif nums[i:] == sorted(nums[i:], reverse = True):
                     i -= 1
-                #if there is something to change.. 
                 else:
-                    del nums[i:]
-                    a = temp.pop(0)
-                    b = min([x for x in temp if x > a])
-                    b_i = temp.index(b)
-                    temp[b_i] = a
-                    temp.sort()
-                    nums.append(b)
-                    nums = nums + temp
+                    a = nums[i] # current head
+                    b = min([x for x in nums[i:] if x > a]) # next head
+                    idx = nums[(i+1):].index(b)
+                    nums[i+1+idx], nums[i] = a, b
+                    nums[(i+1):] = sorted(nums[(i+1):])
                     keep = False
-        return nums
