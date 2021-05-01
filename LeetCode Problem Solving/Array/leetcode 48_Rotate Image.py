edb1a2310916f561efe532e5a39bef8d3ac2629f
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-05-01 13:48:35 +0900

    Update leetcode 48_Rotate Image.py

--- LeetCode Problem Solving/Array/leetcode 48_Rotate Image.py
+++ LeetCode Problem Solving/Array/leetcode 48_Rotate Image.py
@@ -1,3 +1,7 @@
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+# solution 1.
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
@@ -12,3 +16,16 @@ class Solution:
         # flipping matrix horizontally
         for k in range(n):
             matrix[k] = matrix[k][::-1]
+        return matrix
+
+# solution 2. More efficient and simpler approach
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        i = 0
+        for row in zip(*matrix):
+            matrix[i] = list(row)[::-1]
+            i += 1
+        return matrix
